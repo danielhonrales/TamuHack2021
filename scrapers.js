f17b7ec58fbe7e46def853c316c9153ec32b896a
Jireh <jireh.ferrer1@gmail.com>
2021-01-31 08:31:36 -0600

    Gathers URL and Name

--- scrapers.js
+++ scrapers.js
@@ -14,8 +14,6 @@ async function getData(mainIngredient) {
     await page.goto(newUrl);
 
     
-    
-
     // Get all the titles
     const recipeTitles = await page.evaluate( ()=> {
         const titles = document.querySelectorAll('.grd-tile-link');
@@ -30,9 +28,6 @@ async function getData(mainIngredient) {
         return url;
     })
     
-
-    
-
     browser.close();
 
     for (var i = 0; i < recipeTitles.length; i++) {
@@ -40,48 +35,3 @@ async function getData(mainIngredient) {
     }
 
 }
-
-
-/*
-const puppeteer = require('puppeteer');
-
-(async function main() {
-    
-
-    const browser = await puppeteer.launch({headless: false });
-    const page = await browser.newPage();
-
-    // Get main ingredients 
-    var mainIngredient = 'chicken'
-
-    // Get new pages/urls 
-    var parentLink = 'https://www.simplyrecipes.com/recipes/main-ingredient/';
-    var newUrl = parentLink.concat(mainIngredient);
-    await page.goto(newUrl);
-
-    const recipeUrls = await page.evaluate( ()=> {
-        const images = document.querySelectorAll('.grd-tile-link');
-        var url = Array.from(images).map(v=>v.href);
-        return url;
-    })
-
-    var recipeUrlCount = recipeUrls.length;
-    for (var i = 0; i < recipeUrlCount; i++) {
-        await page.goto(recipeUrls[i]);
-
-        const recipe = await page.evaluate( ()=> {
-            const recipeTitle = document.querySelectorAll('.ingredient');
-            var ingredient = Array.from(recipeTitle).map(v=>v.textContent);
-            return ingredient;
-        })
-    }
-
-    browser.close();
-    console.log(recipeUrls[1]);
-        
-})();
-
-
-
-
-*/
